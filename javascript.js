c3aae741c150cbb3da0c8cb5b8941a30a6062b93
luke williams <jluke94williams@gmail.com>
2020-02-18 14:04:43 -0500

    Added Quiz Questions Function and Event Listener

--- javascript.js
+++ javascript.js
@@ -95,8 +95,73 @@ function startTimer () {
         }
     
     }, 1000);
-
+    // Call Function to Render Question when Start Button clicked.
+    renderQuestion()
 }
+
+//Function stored to render new question.
+function renderQuestion () {
+    // Creates New Question Number for Each Individual Question. 
+    questionNumber.textContent = ("Question # " + (questionValue + 1));
+    //Generates next question (Object) in the Array. 
+    var uniquequestion = jsQuestions[questionValue].question;
+    currentQuestion.textContent = uniquequestion;
+    //Generates OptionA in each Object in Array.
+    var pickA = jsQuestions[questionValue].optionA;
+    choiceA.textContent= pickA;
+    //Generates OptionB in each Object in Array.
+    var pickB = jsQuestions[questionValue].optionB;
+    choiceB.textContent = pickB;
+    //Generates OptionC in each Object in Array.
+    var pickC = jsQuestions[questionValue].optionC;
+    choiceC.textContent= pickC;
+    //Generates OptionD in each Object in Array. 
+    var pickD = jsQuestions[questionValue].optionD;
+    choiceD.textContent = pickD;
+
+    //Changes Quiz Questions CSS to Visible.
+    quizQuestions.setAttribute("style", "visibility: visible");
+    //Changes Start Button CSS to Hidden. 
+    start.setAttribute("style", "visibility: hidden");
+    //Changes Next Question Button CSS to Visible.
+    newQuestion.setAttribute("style", "visibility: visible");
+
+    // When Question Number is equal to 4 the Next Question Button is Hidden and Finish Button is Visible.
+    if(questionValue === nQuestions - 1) {
+        finish.setAttribute("style", "visibility: visible");
+        newQuestion.setAttribute("style", "visibility: hidden")
+    };
+
+    
+
+};
+
+//Stored Function to cyle to next question when Next Question button is clicked. 
+function nextQuestion() {
+
+    //Stores the quiz takers input.
+    var checkedOption = document.querySelector('input[type=radio]:checked');
+    var answer = checkedOption.value
+    // When the stored quiz takers answer is equal to arrays object answer score incrrease by 1, question number increases by 1 and user is alerted of correct answer. 
+    if(jsQuestions[questionValue].correct == answer){
+    score+=1;
+    questionValue++;
+    alert("That was correct!");
+}
+    //When the stored quiz takers answer is not equal to array ojects answer question value increase by 1 and user is alerted of incorrect answer. 
+    else{
+    questionValue++;
+    timeRemaining-=10;
+    alert("Sorry that was incorrect!");
+}
+    //Calls function to render question. 
+    renderQuestion()
+
+};
+
+
 
 //Start Timer Event Listener.
 start.addEventListener("click", startTimer);
+//New Question Event Listener
+newQuestion.addEventListener("click", nextQuestion);
