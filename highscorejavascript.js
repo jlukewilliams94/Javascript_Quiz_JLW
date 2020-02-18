08d225c1eedbf0d9c8513b6503fef8398c1e0820
luke williams <jluke94williams@gmail.com>
2020-02-18 14:22:40 -0500

    HighScores JS to retrieve HS and display as OL

--- highscorejavascript.js
+++ highscorejavascript.js
@@ -1,10 +1,10 @@
+//Stores ID from document as a variable
 var highScoreList = document.querySelector("#highScoreList");
+//Retrieved highscores from local storage as a variable. 
 var highScores= JSON.parse(localStorage.getItem("highscores")) || [];
 
 
-console.log(highScores)
-
-
+//For Loop to generate a ordered list of highscores. 
 for (var i = 0; i < highScores.length; i++) {
     var listItem = document.createElement("li");
     listItem.textContent = highScores[i].name + "  " +highScores[i].points;
